03d218fe79dd6a68bc4956d924030bdb79663ea1
drkitty <bumblingkitty@gmail.com>
2017-10-24 11:43:06 -0700

    Use naive ordering algo; find counterexample

--- test.py
+++ test.py
@@ -5,21 +5,39 @@ class Restructor(object):
     def __init__(self):
         self.rules = {}
 
-    def __str__(self):
-        return "\n".join(repr(rule) for rule in self.rules.values())
-
     def add(self, target, prereqs, recipe):
         self.rules[target] = Rule(target, prereqs, recipe)
 
-    def do(self, target):
+    #def add_indirect(self, target, prereqs, recipe):
+
+    def get_order(self, target):
         for rule in self.rules.values():
             for i, prereq in enumerate(rule.prereqs):
                 if isinstance(prereq, str):
                     x = self.rules.get(prereq, None)
                     if x is not None:
-                        print(x)
                         rule.prereqs[i] = x
-        raise Exception("lazy cat")
+
+
+        start = self.rules[target]
+        order = []
+        queue = [start]
+        visited = set()
+        while queue:
+            queue2 = []
+            for rule in queue:
+                if isinstance(rule, str):
+                    continue
+                if rule not in visited:
+                    order.append(rule)
+                    queue2.extend(rule.prereqs)
+                elif rule is start:
+                    raise Exception("Prereq cycle")
+            visited.update(queue)
+            queue = queue2
+        order.reverse()
+
+        return order
 
 
 class Rule(object):
@@ -29,7 +47,10 @@ class Rule(object):
         self.recipe = recipe
 
     def __repr__(self):
-        return "{} -> {}".format(repr(self.prereqs), self.target)
+        return repr(self.target)
+
+    def __str__(self):
+        return "{} -> {}".format(repr(self.prereqs), repr(self.target))
 
 
 GCC1 = 'gcc -std=c99 -pedantic -Wall -Wextra -Werror'
@@ -38,8 +59,6 @@ GCC1 = 'gcc -std=c99 -pedantic -Wall -Wextra -Werror'
 r = Restructor()
 r.add('foo', ('foo.c', 'foo.h'), GCC1 + ' -o foo foo.c')
 r.add('foo.h', ('foo.h.in',), 'cp foo.h.in foo.h')
-print(r)
-try:
-    r.do('hey')
-except: pass
-print(r)
+r.add('foo.c', ('foo.h',), '')
+#r.add('foo.h.in', ('foo',), 'echo oops')
+print(r.get_order('foo'))
