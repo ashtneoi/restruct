f193d86ef6cb71dd4c7537ecefa5d8a773bbf5a3
drkitty <bumblingkitty@gmail.com>
2017-10-23 23:27:15 -0700

    We'll see where this goes

---
+++ foo.c
@@ -0,0 +1,7 @@
+#include "foo.h"
+
+
+int main(void)
+{
+    fputs("Hello, world!\n", stdout);
+}
---
+++ test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+
+class Restructor(object):
+    def __init__(self):
+        self.rules = {}
+
+    def __str__(self):
+        return "\n".join(repr(rule) for rule in self.rules.values())
+
+    def add(self, target, prereqs, recipe):
+        self.rules[target] = Rule(target, prereqs, recipe)
+
+    def do(self, target):
+        for rule in self.rules:
+            for i, prereq in enumerate(rule.prereqs):
+                if isinstance(prereqe, str):
+                    x = self.rules.get(prereq, None)
+                    if x is not None:
+                        rule.prereqs[i] = x
+        raise Exception("lazy cat")
+
+
+class Rule(object):
+    def __init__(self, target, prereqs, recipe):
+        self.target = target
+        self.prereqs = list(prereqs)
+        self.recipe = recipe
+
+    def __repr__(self):
+        return "{} -> {}".format(
+            repr(tuple(
+                (p if isinstance(p, str) else p.target) for p in self.prereqs
+            )),
+            self.target,
+        )
+
+
+GCC1 = 'gcc -std=c99 -pedantic -Wall -Wextra -Werror'
+
+
+r = Restructor()
+r.add('foo', ('foo.c', 'foo.h'), GCC1 + ' -o foo foo.c')
+r.add('foo.h', ('foo.h.in',), 'cp foo.h.in foo.h')
+print(r)
