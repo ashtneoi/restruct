785baa72fe9a50a6a732c840b37154d45866281b
drkitty <bumblingkitty@gmail.com>
2017-10-29 19:20:52 -0700

    I think it's working

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 
+import os
+from datetime import datetime
+from math import inf
+
+
 class Restructor(object):
     def __init__(self, shell='bash'):
         self.rules = {}
@@ -11,13 +16,22 @@ class Restructor(object):
             raise Exception("Target '{}' already has a rule".format(target))
         self.rules[target] = Rule(target, prereqs, recipe)
 
+    def get_stamp(self, x):
+        try:
+            return os.stat(x.target).st_mtime
+        except FileNotFoundError:
+            return -inf
+
     def get_order(self, target):
-        for rule in self.rules.values():
+        for rule in tuple(self.rules.values()):
             for i, prereq in enumerate(rule.prereqs):
                 if isinstance(prereq, str):
-                    x = self.rules.get(prereq, None)
-                    if x is not None:
-                        rule.prereqs[i] = x
+                    prereq_name = prereq
+                    prereq = self.rules.get(prereq_name)
+                    if prereq is None:
+                        prereq = self.rules[prereq_name] = Leaf(prereq_name)
+                    rule.prereqs[i] = prereq
+                prereq.products.add(rule)
 
         start = self.rules[target]
         order = {}
@@ -28,14 +42,11 @@ class Restructor(object):
             next_frontier = set()
             for rule in frontier:
                 print(" " + repr(rule))
-                if isinstance(rule, str):
+                if rule not in visited:
                     order[rule] = d
-                else:
-                    if rule not in visited:
-                        order[rule] = d
-                        next_frontier.update(rule.prereqs)
-                    elif rule is start:
-                        raise Exception("Prereq cycle")
+                    next_frontier.update(rule.prereqs)
+                elif rule is start:
+                    raise Exception("Prereq cycle")
             visited.update(frontier)
             frontier = next_frontier
             d += 1
@@ -46,11 +57,33 @@ class Restructor(object):
 
         return order
 
+    def do(self, target, *, dryrun=True):
+        if not dryrun:
+            raise Exception("very lazy cat")
 
-class Rule(object):
+        order = self.get_order(target)
+        now = datetime.now().timestamp()
+
+        dirty = set()
+        for rule in order:
+            if rule in dirty or any(
+                    self.get_stamp(rule) < self.get_stamp(prereq)
+                    for prereq in rule.prereqs
+            ):
+                print(rule)
+                dirty.update(rule.products)
+
+
+class Node(object):
+    def __hash__(self):
+        return hash(self.target)
+
+
+class Rule(Node):
     def __init__(self, target, prereqs, recipe=None):
         self.target = target
         self.prereqs = list(prereqs)
+        self.products = set()  # opposite of prereqs
         self.recipe = recipe
 
     def __repr__(self):
@@ -60,10 +93,30 @@ class Rule(object):
         return "{} -> {}".format(repr(self.prereqs), repr(self.target))
 
 
+class Leaf(Node):
+    prereqs = ()
+
+    def __init__(self, target):
+        self.target = target
+        self.products = set()
+
+    def __repr__(self):
+        return "<" + repr(self.target) + ">"
+
+
 GCC1 = 'gcc -std=c99 -pedantic -Wall -Wextra -Werror'
 
 
-r = Restructor()
-r.add('c', ('b', 'a'))
-r.add('b', ('a',))
-print(r.get_order('c'))
+def test_order():
+    r = Restructor()
+    r.add('c', ('b', 'a'))
+    r.add('b', ('a',))
+    print(r.get_order('c'))
+
+def test_stat():
+    r = Restructor()
+    r.add('c', ('b', 'a'))
+    r.add('b', ('a',))
+    r.do('c')
+
+test_stat()
